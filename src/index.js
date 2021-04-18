35452a245b46b085901c0216740a794b0222de27
Michelle Ranly <meranly8@gmail.com>
2021-04-17 23:47:17 -0500

    Declared fetchWines() & renderWines()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 const baseURL = "http://localhost:3000"
 const winesList = document.querySelector("#wines-list")
 const addWineBtn = document.querySelector("#add-wine-btn")
+
+function fetchWines() {
+    fetch(baseURL + "/wines")
+    .then(resp => resp.json())
+    .then(winesJSON => {
+        renderWines(winesJSON)
+    })
+}
+
+function renderWines(json) {
+    const wines = json["data"]
+    wines.map(wine => {
+        const li = document.createElement("li")
+        li.innerHTML = `
+            <h4>${wine.attributes.wine}<br></h4>
+            <h5>${wine.attributes.region}, ${wine.attributes.country}<br>${wine.attributes.year}</h5>
+            <img src="${wine.attributes.image}">
+            <p></p>
+        `
+    winesList.append(li)
+    })
+}
+
+fetchWines()
