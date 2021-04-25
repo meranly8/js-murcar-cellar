b557a3fa258445e62ce9935ed34737c974f34f24
Michelle Ranly <meranly8@gmail.com>
2021-04-25 02:13:32 -0500

    Changed Comment#constructor() to create 'p' & removed #addToTable()

--- src/comment.js
+++ src/comment.js
@@ -8,24 +8,10 @@ class Comment {
         this.wine_id = wine_id
         this.created_at = created_at
 
-        // only comments with unique wine ids need to have a th
-        this.th = document.createElement("th")
-        this.th.dataset.id = wine_id
+        this.p = document.createElement("p")
+        this.p.id = `comment-${id}`
+        this.p.dataset.id = wine_id
 
         Comment.all.push(this)
-    }
-
-    // need comments with the same wine_id to be added to the same th
-
-    addToTable() {
-        
-        
-        debugger
-        document.querySelector(`tr#wine-${this.id}`).innerHTML += `
-            <h5>Created at: ${this.created_at}</h5>
-            <h5>By: ${this.name}</h5>
-            <h6>${this.wine_id}</h6>
-            <p>${this.comment}</p>
-        `
     }
 }
