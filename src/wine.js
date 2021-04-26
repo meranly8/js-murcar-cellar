1e6d77050d7729ac6a166f487c386c61ca2264bf
Michelle Ranly <meranly8@gmail.com>
2021-04-25 21:18:11 -0500

    Moved wine's edit form into Wine#renderEditForm()

--- src/wine.js
+++ src/wine.js
@@ -29,50 +29,16 @@ class Wine {
     }
 
     renderWineTr(){
-        const checked = (this.opened === true) ? "checked" : ""
-
         this.tr.innerHTML = `
             <th id="${this.id}" class="wine-details">
                 <h3>${this.wine}<br>
                     ${this.region}, ${this.country} (${this.year})
                 </h3>
+
                 <button id="update-${this.id}-button" data-id=${this.id}>Update Details</button><br><br>
                 <div id="edit-form-${this.id}-div" class="form-closed">
-                    <form id="edit-form-${this.id}" data-id=${this.id}>
-                        <table>
-                            <th>
-                            <label>Wine:
-                                <input type="text" name="wine" id="edit-wine-${this.id}" class="edit-form-column-1" value="${this.wine}"><br>
-                            </label><br>
-                            <label>Region:
-                                <input type="" name="region" id="edit-region-${this.id}" class="edit-form-column-1" value="${this.region}"><br>
-                            </label><br>
-                            <label>Country:
-                                <input type="" name="country" id="edit-country-${this.id}" class="edit-form-column-1" value="${this.country}"><br>
-                            </label><br>
-                            <label>Year:
-                                <input type="number" name="year" id="edit-year-${this.id}" min=1900 class="edit-form-column-1" value="${this.year}"><br>
-                            </label>
-                            </th>
-                            <th>
-                            <label >Price:
-                                $<input type="number" name="price" id="edit-price-${this.id}" class="edit-form-column-2" value=${this.price}><br>
-                            </label><br>
-                            <label>Opened:
-                                <input type="checkbox" name="opened" id="edit-opened-${this.id}" class="edit-form-column-2" value=${this.opened} ${checked}><br>
-                            </label><br>
-                            <label>Rating:
-                                <input type="number" name="rating" id="edit-rating-${this.id}" min=0 max=5 step=.5 class="edit-form-column-2" value=${this.rating}><br>
-                            </label><br>
-                            <label>Label Image Link:
-                                <input type="text" name="image" id="edit-image-${this.id}" placeholder="ex: https://i.imgur.com/Hnlo6p8h.png" class="edit-form-column-2" value="${this.image}"><br><br>
-                            </label>
-                            </th>
-                        </table>
-                        <input type="submit" value="Update Wine">
-                    </form><br>
-                    <button id="delete-${this.id}-button" data-id=${this.id}>Delete Wine</button><br><br>
                 </div>
+
                 <b>Price:</b> $${this.price}<br>
                 <b>Opened:</b> <container id="opened-detail-${this.id}">${this.opened}</container><br>
                 <b>Rating:</b> <container id="rating-detail-${this.id}">${this.rating}</container><br><br>
@@ -167,14 +133,55 @@ class Wine {
         Wine.displayTotal()
     }
 
+    renderEditForm = () => {
+        const form = this.tr.querySelector(`#edit-form-${this.id}-div`)
+        const checked = (this.opened === true) ? "checked" : ""
+        form.innerHTML = `
+        <form id="edit-form-${this.id}" data-id=${this.id}>
+            <table>
+                <th>
+                <label>Wine:
+                    <input type="text" name="wine" id="edit-wine-${this.id}" class="edit-form-column-1" value="${this.wine}"><br>
+                </label><br>
+                <label>Region:
+                    <input type="" name="region" id="edit-region-${this.id}" class="edit-form-column-1" value="${this.region}"><br>
+                </label><br>
+                <label>Country:
+                    <input type="" name="country" id="edit-country-${this.id}" class="edit-form-column-1" value="${this.country}"><br>
+                </label><br>
+                <label>Year:
+                    <input type="number" name="year" id="edit-year-${this.id}" min=1900 class="edit-form-column-1" value="${this.year}"><br>
+                </label>
+                </th>
+                <th>
+                <label >Price:
+                    $<input type="number" name="price" id="edit-price-${this.id}" class="edit-form-column-2" value=${this.price}><br>
+                </label><br>
+                <label>Opened:
+                    <input type="checkbox" name="opened" id="edit-opened-${this.id}" class="edit-form-column-2" value=${this.opened} ${checked}><br>
+                </label><br>
+                <label>Rating:
+                    <input type="number" name="rating" id="edit-rating-${this.id}" min=0 max=5 step=.5 class="edit-form-column-2" value=${this.rating}><br>
+                </label><br>
+                <label>Label Image Link:
+                    <input type="text" name="image" id="edit-image-${this.id}" placeholder="ex: https://i.imgur.com/Hnlo6p8h.png" class="edit-form-column-2" value="${this.image}"><br><br>
+                </label>
+                </th>
+            </table>
+            <input type="submit" value="Update Wine">
+        </form><br>
+        <button id="delete-${this.id}-button" data-id=${this.id}>Delete Wine</button><br><br>
+        `
+    }
+
     showEditForm = (event) => {
         const editDiv = event.target.nextSibling.nextElementSibling.nextElementSibling
-            editDiv.className = "form-opened"
-            
-            const editForm = editDiv.querySelector(`#edit-form-${this.id}`)
-            editForm.addEventListener('submit', this.handleWineUpdateSubmit)
+        editDiv.className = "form-opened"
+        
+        const editForm = editDiv.querySelector(`#edit-form-${this.id}`)
+        editForm.addEventListener('submit', this.handleWineUpdateSubmit)
 
-            event.target.innerText = "Close Edit Form"
+        event.target.innerText = "Close Edit Form"
     }
 
     showComments = (event) => {
