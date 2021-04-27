7d0df2e4e17d272ff1c6b88808d8772538f9f797
Michelle Ranly <meranly8@gmail.com>
2021-04-27 17:07:19 -0500

    Moved Comment#renderCommentForm(), #handleNewCommentSubmit(), #hideCommentForm() into Wine

--- src/comment.js
+++ src/comment.js
@@ -34,22 +34,6 @@ class Comment {
         return this.p
     }
 
-    renderCommentForm() {
-        const commentForm = document.querySelector(`#add-cmt-wine-${this.wine_id}`)
-        commentForm.innerHTML = `
-        <br>
-        <label>
-            Name: <input type="text" name="comment-name" id="comment-name-${this.wine_id}">
-        </label><br>
-        <label>
-            Comment: <textarea name="comment-comment" id="comment-comment-${this.wine_id}"></textarea>
-        </label>
-        <input type="hidden" name="comment-wine_id" id="comment-wine_id-${this.wine_id}" value="${this.wine_id}"><br>
-        <input type="submit" value="Create Comment">
-        `
-        commentForm.addEventListener('submit', this.handleNewCommentSubmit)
-    }
-
     formatCreatedAt() {
         const D = new Date(this.created_at)
         const options = {
@@ -62,31 +46,6 @@ class Comment {
         
         const dateTime = this.p.querySelector(`#comment-${this.id}-created-at`)
         dateTime.innerText = I
-    }
-
-    handleNewCommentSubmit = (event) => {
-        event.preventDefault()
-        const nameValue = document.querySelector(`#comment-name-${this.wine_id}`).value
-        const commentValue = document.querySelector(`#comment-comment-${this.wine_id}`).value
-        const wineIdValue = document.querySelector(`#comment-wine_id-${this.wine_id}`).value
-
-        this.name = nameValue
-        this.comment = commentValue
-        this.wine_id = wineIdValue
-
-        const commentForm = document.querySelector(`#add-cmt-wine-${this.wine_id}`)
-        commentForm.reset()
-        this.hideCommentForm(event)
-        
-        CommentApi.createComment(this)
-    }
-
-    hideCommentForm = (event) => {
-        const commentForm = event.target
-        commentForm.className = "form-closed"
-
-        const commentFormBtn = event.target.previousElementSibling
-        commentFormBtn.innerText = "Add Comment"
     }
 
     handleCommentDelete() {
