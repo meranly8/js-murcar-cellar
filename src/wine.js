b7635e68a689dc49bbee460ef4e1fec78be2c8e1
Michelle Ranly <meranly8@gmail.com>
2021-04-30 02:55:06 -0500

    Changed order to Wine static methods

--- src/wine.js
+++ src/wine.js
@@ -21,20 +21,8 @@ class Wine {
         Wine.all.push(this)
     }
 
-    updateDOM() {
-        Wine.sortTable()
-        Wine.displayTotal()
-        Wine.renderWineTOC()
-    }
-
-    static sortTable() {
-        wineTable.innerHTML = ""
-        
-        const wines = Wine.alphaSortWine()
-        wines.map(wine => {
-            wineTable.appendChild(wine.renderWineTr())
-            wine.detailsFormatting()
-        })
+    setWineCommentsOnDOM() {
+        CommentApi.fetchWinesComments(this)
     }
 
     renderWineTr() {
@@ -125,84 +113,6 @@ class Wine {
         createdAt.innerText = shortDate
     }
 
-    static displayTotal() {
-        wineTotal.innerText = Wine.all.length
-        Wine.all.length === 0 ? countryTotalsBtn.className = 'form-closed' : countryTotalsBtn.className = 'form-open'
-    }
-
-    static renderWineTOC() {
-        wineTOCList.innerHTML = ""
-        
-        if (Wine.all.length === 0) {
-            wineTOCList.innerHTML = "No wines in collection yet."
-        } else {
-            const wines = this.alphaSortWine()
-            
-            wines.map(wine => {
-                const li = document.createElement("li")
-                li.id = wine.id + "-link-li"
-                li.innerHTML = `
-                    <a href="#${wine.id}" id="${wine.id}-link">${wine.wine}, ${wine.country} (${wine.year})</a>
-                `
-                wineTOCList.append(li)
-            })
-        }
-    }
-
-    static alphaSortWine = () => {
-        return Wine.all.sort((a, b) => {
-            let nameA = a.wine.toUpperCase()
-            let nameB = b.wine.toUpperCase()
-            if (nameA < nameB) {
-                return -1
-            } else {
-                return 1
-            }
-        })
-    }
-
-    static sortWineYear() {
-        return Wine.all.sort((a, b) => {
-            return a.year - b.year
-        })
-    }
-
-    static sortWinePrice() {
-        return Wine.all.sort((a, b) => {
-            return b.price - a.price
-        })
-    }
-
-    static alphaSortCountry() {
-        return Wine.all.sort((a, b) => {
-            let countryA = a.country.toUpperCase()
-            let countryB = b.country.toUpperCase()
-            if (countryA < countryB) {
-                return -1
-            } else {
-                return 1
-            }
-        })
-    }
-
-    static sortFilterDOMBy(wines) {
-        wineTable.innerHTML = ""
-        wineTOCList.innerHTML = ""
-
-        wines.map(wine => {
-            wineTable.appendChild(wine.renderWineTr())
-            
-            wine.detailsFormatting()
-
-            const li = document.createElement("li")
-            li.id = wine.id + "-link-li"
-            li.innerHTML = `
-                <a href="#${wine.id}" id="${wine.id}-link">${wine.wine}, ${wine.country} (${wine.year})</a>
-            `
-            wineTOCList.append(li)
-        })
-    }
-
     handleWineUpdateSubmit = (event) => {
         event.preventDefault()
         
@@ -216,6 +126,7 @@ class Wine {
         this.image = this.tr.querySelector(`#edit-image-${this.id}`).value
 
         WineApi.updateWine(this)
+
     }
 
     renderEditForm = () => {
@@ -282,8 +193,9 @@ class Wine {
         const index = wines.indexOf(this)
         wines.splice(index, 1)
 
-        Wine.renderWineTOC()
         Wine.displayTotal()
+        Wine.addFilterOptions()
+        Wine.renderWineTOC()
     }
 
     showWineComments = (event) => {
@@ -357,5 +269,126 @@ class Wine {
 
         const addCommentBtn = this.tr.querySelector(`#wine-${this.id}-add-cmt-btn`)
         addCommentBtn.innerText = "Add Comment"
+    }
+
+    static updateDOM() {
+        Wine.setTable()
+        Wine.displayTotal()
+        Wine.addFilterOptions()
+        Wine.renderWineTOC()
+       
+        CommentApi.fetchComments()
+    }
+
+    static setTable() {
+        wineTable.innerHTML = ""
+        
+        this.alphaSortWine().map(wine => {
+            wineTable.appendChild(wine.renderWineTr())
+            wine.detailsFormatting()
+        })
+    }
+
+    static displayTotal() {
+        wineTotal.innerText = Wine.all.length
+        Wine.all.length === 0 ? countryTotalsBtn.className = 'form-closed' : countryTotalsBtn.className = 'form-open'
+    }
+
+    static addFilterOptions() {
+        filterList.innerHTML = ""
+        
+        const distinctCountries = [...new Set(Wine.all.map(wine => wine.country))] 
+    
+        distinctCountries.map(country => {
+            const option = document.createElement("option")
+            option.text = country
+            filterList.add(option)
+        })
+    
+        const allWines = document.createElement("option")
+        allWines.text = "All Wines"
+        allWines.selected = true
+        filterList.add(allWines, 0)
+    
+        const opened = document.createElement("option")
+        opened.text = "Opened"
+        filterList.add(opened, 1)
+    
+        const unopened = document.createElement("option")
+        unopened.text = "Unopened"
+        filterList.add(unopened, 2)
+    }
+
+    static renderWineTOC() {
+        wineTOCList.innerHTML = ""
+        
+        if (Wine.all.length === 0) {
+            wineTOCList.innerHTML = "No wines in collection yet."
+        } else {
+            const wines = this.alphaSortWine()
+            
+            wines.map(wine => {
+                const li = document.createElement("li")
+                li.id = wine.id + "-link-li"
+                li.innerHTML = `
+                    <a href="#${wine.id}" id="${wine.id}-link">${wine.wine}, ${wine.country} (${wine.year})</a>
+                `
+                wineTOCList.append(li)
+            })
+        }
+    }
+
+    static alphaSortWine = () => {
+        return Wine.all.sort((a, b) => {
+            let nameA = a.wine.toUpperCase()
+            let nameB = b.wine.toUpperCase()
+            if (nameA < nameB) {
+                return -1
+            } else {
+                return 1
+            }
+        })
+    }
+
+    static sortWineYear() {
+        return Wine.all.sort((a, b) => {
+            return a.year - b.year
+        })
+    }
+
+    static sortWinePrice() {
+        return Wine.all.sort((a, b) => {
+            return b.price - a.price
+        })
+    }
+
+    static alphaSortCountry() {
+        return Wine.all.sort((a, b) => {
+            let countryA = a.country.toUpperCase()
+            let countryB = b.country.toUpperCase()
+            if (countryA < countryB) {
+                return -1
+            } else {
+                return 1
+            }
+        })
+    }
+
+    static sortFilterDOMBy(wines) {
+        wineTable.innerHTML = ""
+        wineTOCList.innerHTML = ""
+
+        wines.map(wine => {
+            wineTable.appendChild(wine.renderWineTr())
+            
+            wine.detailsFormatting()
+
+            const li = document.createElement("li")
+            li.id = wine.id + "-link-li"
+            li.innerHTML = `
+                <a href="#${wine.id}" id="${wine.id}-link">${wine.wine}, ${wine.country} (${wine.year})</a>
+            `
+            wineTOCList.append(li)
+        })
     }
 }
